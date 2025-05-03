b681e612f0863cb55b636a04b5f41a75524c54c7
himanshu101 <himanshukumar.connect@gmail.com>
2025-05-04 00:43:54 +0530

    deploy

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,16 +1,25 @@
+
 const express = require("express");
 const aiRoutes = require("./routes/ai.routes");
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 
-app.use(cors())
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
+
+app.options('*', cors());
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Hello world");
 });
+
 
 app.use("/ai", aiRoutes);
 
